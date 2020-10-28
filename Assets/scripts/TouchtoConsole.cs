4fe6943a16c1fced6962040dc4c02d769d7ee1ef
Basti <73500107+Bowet@users.noreply.github.com>
2020-10-28 21:09:56 +0100

    Mal schauen

---
+++ Assets/scripts/TouchtoConsole.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchtoConsole : MonoBehaviour
+{
+    public int i;
+    public Text CountText;
+
+
+    private void Start()
+    {
+       // CountText = GetComponent<Text>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Touch peter in Input.touches)
+        {
+            if (peter.phase == TouchPhase.Began)
+            {
+                i++;
+            }
+        }
+        CountText.text = i.ToString();
+    }
+}
